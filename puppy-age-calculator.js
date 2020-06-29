8b18577910be87a90cf6b490f41b800dd86f085d
Ruben80 <43403734+Ruben80@users.noreply.github.com>
2020-06-29 21:00:00 +0200

    Add files via upload

---
+++ puppy-age-calculator.js
@@ -0,0 +1,45 @@
+const calculateDogAge = function (age) {
+  let dogYears = "Your doggie is " + age * 7 + " years old in human years!";
+  console.log(dogYears);
+};
+
+calculateDogAge("1");
+calculateDogAge("0.5");
+calculateDogAge("0.1");
+
+// const calculateDogAge = function(age) {
+//     let dogYears = 7 * age;
+//     console.log("Your doggie is " + dogYears + " years old in dog years!");
+//   };
+  
+//   calculateDogAge(1);
+//   calculateDogAge(0.5);
+//   calculateDogAge(12);
+
+
+// const tellFortune = function(jobTitle, geoLocation, partner, numKids){
+//     return ("You will be a " + jobTitle + "at " + geoLocation + " with " + partner + "with " + numKids + " kids.");
+// };
+
+// console.log(tellFortune("Milionair", "Odessa", "Lucy", "3"));
+// console.log(tellFortune("Bum", "Amsterdam", "Many", "12"));
+// console.log(tellFortune("Programmer", "Arnhem", "Meggie", "2"));
+
+// const tellFortune = function(jobTitle, geoLocation, partner, numKids) {
+//     let future =
+//       "You will be a " +
+//       jobTitle +
+//       " in " +
+//       geoLocation +
+//       " and married to " +
+//       partner +
+//       " " +
+//       " with " +
+//       numKids +
+//       " kids.";
+//     console.log(future);
+//   };
+
+//   tellFortune("bball player", "spain", "Shaq", 3);
+//   tellFortune("stunt double", "Japan", "Ryan Gosling", 3000);
+//   tellFortune("Elvis impersonator", "Russia", "The Oatmeal", 0);
